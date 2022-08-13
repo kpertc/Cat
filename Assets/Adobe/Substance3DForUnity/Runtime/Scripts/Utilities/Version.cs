aa34e42ca9a02b17cadd07871ede3d01c359c997
陈憬夫 <chenjingfu.design@bytedance.com>
2022-08-13 19:54:18 +0800

    First Commit

---
+++ Assets/Adobe/Substance3DForUnity/Runtime/Scripts/Utilities/Version.cs
@@ -0,0 +1,9 @@
+namespace Adobe.Substance
+{
+    public static class Version
+    {
+        public const string PluginVersion = "3.1.0-0-g3673e7e";
+        public const string EngineVersion = "8.2.0 0138644d";
+        public const int ImporterVersion = 310;
+    }
+}
