4c7432eb30031f7b26a09be77c46925596523fb8
陈憬夫 <chenjingfu.design@bytedance.com>
2022-08-20 21:55:52 +0800

    complete cat texture

---
+++ Assets/Cat/eyeControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Adobe.Substance.Runtime;
+
+public class eyeControl : MonoBehaviour
+{
+    public Adobe.Substance.Runtime.SubstanceRuntimeGraph mySubstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
